7b0e459b78b1cabdacd146cced670284ca688335
jainex17 <jainexp017@gmail.com>
2024-01-31 20:09:24 +0530

    Add template and static folders to Flask app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 # from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 # db = SQLAlchemy(app)
 
