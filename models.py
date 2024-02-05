ce71eed0c3da863736e566641ad16fbddd273ed2
jainex17 <jainexp017@gmail.com>
2024-02-05 11:33:51 +0530

    Update models and routes for user authentication and stock dashboard

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     
     def __init__(self, username, email, password):
@@ -15,6 +15,4 @@ class User(db.Model):
 
     def check_password(self, enterpassword):
         stored_password_bytes = bytes(self.password)
-        print('Stored Password Bytes', stored_password_bytes)
         return bcrypt.checkpw(enterpassword.encode('utf-8'), stored_password_bytes)
-        
--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, request, redirect, session
 from models import User
 from app import db, app
+import requests
+import os
+import plotly.graph_objs as go
+from datetime import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def register():
@@ -39,19 +43,61 @@ def login():
                 return render_template('./login.html', error="Invalid email or password")
 
         return render_template('./login.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('email', None)
+    return redirect('/login')
 
-@app.route('/dashboard')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     if 'username' in session:
-        # stock_symbol = 'IBM'
+        stock_symbol = 'IBM'
+        if request.method == 'POST':
+            stock_symbol = request.form['stock']
+            
+            graphJSON = get_stock_data(stock_symbol)
+            return render_template('./dashboard.html', graphJSON=graphJSON, stock_symbol=stock_symbol)
 
-        # api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={os.environ.get("API_KEY")}'
-        
-        # response = requests.get(api_url)
-        # data = response.json()['Time Series (5min)']
-        # print(data)
-     
-        return render_template('./dashboard.html')
+        graphJSON = get_stock_data(stock_symbol)
+        return render_template('./dashboard.html', graphJSON=graphJSON, stock_symbol=stock_symbol)
+    else:
+        return redirect('/login')
+    
+@app.route('/dashboard/subscribe', methods=['GET', 'POST'])
+def dashboard_subscribe():
+    if 'username' in session:
+        return redirect('/dashboard')
     else:
         return redirect('/login')
 
+def get_stock_data(stock_symbol):
+        api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol}&interval=5min&apikey={os.environ.get("API_KEY")}'
+        
+        response = requests.get(api_url)
+        if response.status_code != 200:
+            return redirect('/login', error="Sorry error fetching data")
+        data = response.json()['Time Series (5min)']
+        
+        timestamps = [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') for timestamp in data.keys()]
+        closing_prices = [float(entry['4. close']) for entry in data.values()]
+
+        plotly_fig = go.Figure()
+
+        # Add trace
+        plotly_trace = go.Scatter(x=timestamps, y=closing_prices, mode='lines+markers', name='Closing Price')
+        plotly_fig.add_trace(plotly_trace)
+
+        # Layout adjustments
+        plotly_fig.update_layout(
+            title=f'{stock_symbol} Stock Price (5min intervals)',
+            xaxis_title='Timestamp',
+            yaxis_title='Closing Price'
+        )
+
+        # Create graphJSON
+        graphJSON = plotly_fig.to_json()
+
+        return graphJSON
