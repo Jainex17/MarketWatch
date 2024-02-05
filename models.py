09777167e22f4331e1bc1ad693bf04df78c9811b
jainex17 <jainexp017@gmail.com>
2024-02-05 16:32:46 +0530

    Add Subscribe_stock model and Watchlist template

--- models.py
+++ models.py
@@ -16,3 +16,20 @@ class User(db.Model):
     def check_password(self, enterpassword):
         stored_password_bytes = bytes(self.password)
         return bcrypt.checkpw(enterpassword.encode('utf-8'), stored_password_bytes)
+
+class Subscribe_stock(db.Model):
+    __tablename__ = 'subscribe_stock'
+    id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def __init__(self, stock_symbol, user_id):
+        self.stock_symbol = stock_symbol
+        self.user_id = user_id
+
+    def check_already_subscribed(self, stock_symbol, user_id):
+        checkval = Subscribe_stock.query.filter_by(stock_symbol=stock_symbol, user_id=user_id).first()
+        if checkval:
+            return True
+        else:  
+            return False
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, request, redirect, session
-from models import User
+from flask import render_template, request, redirect, session, url_for
+from models import User, Subscribe_stock
 from app import db, app
 import requests
 import os
@@ -69,35 +69,71 @@ def dashboard():
 @app.route('/dashboard/subscribe', methods=['GET', 'POST'])
 def dashboard_subscribe():
     if 'username' in session:
+        if request.method == 'POST':
+            stock_symbol = request.form['stock']
+            user = User.query.filter_by(email=session['email']).first()
+            new_subscription = Subscribe_stock(stock_symbol=stock_symbol, user_id=user.id)
+            
+            # return true if alredy exist else false
+            if new_subscription.check_already_subscribed(stock_symbol, user.id):
+                return redirect('/dashboard', error="Already subscribed")
+            else:
+                db.session.add(new_subscription)
+                db.session.commit()
+            
         return redirect('/dashboard')
+        
+    else:
+        return redirect('/login')
+
+@app.route('/dashboard/unsubscribe/<stock_symbol>', methods=['GET', 'POST'])
+def dashboard_unsubscribe(stock_symbol):
+    if 'username' in session:
+        print(stock_symbol)
+        user = User.query.filter_by(email=session['email']).first()
+        subscription = Subscribe_stock.query.filter_by(stock_symbol=stock_symbol, user_id=user.id).first()
+        print(subscription)
+        db.session.delete(subscription)
+        db.session.commit()
+        return redirect('/dashboard/watchlist')
+    else:
+        return redirect('/login')
+
+@app.route('/dashboard/watchlist', methods=['GET', 'POST'])
+def user_watchlist():
+    if 'username' in session:
+        user = User.query.filter_by(email=session['email']).first()
+        stock_list = Subscribe_stock.query.filter_by(user_id=user.id).all()
+        return render_template('./watchlist.html', stock_list=stock_list)
     else:
         return redirect('/login')
 
 def get_stock_data(stock_symbol):
         api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol}&interval=5min&apikey={os.environ.get("API_KEY")}'
-        
-        response = requests.get(api_url)
-        if response.status_code != 200:
-            return redirect('/login', error="Sorry error fetching data")
-        data = response.json()['Time Series (5min)']
-        
-        timestamps = [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') for timestamp in data.keys()]
-        closing_prices = [float(entry['4. close']) for entry in data.values()]
+        # api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock_symbol}&interval=5min&apikey=demo'
+        try:
+            response = requests.get(api_url)
+            data = response.json()['Time Series (5min)']
+            print("Data: ", data)
+            timestamps = [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') for timestamp in data.keys()]
+            closing_prices = [float(entry['4. close']) for entry in data.values()]
 
-        plotly_fig = go.Figure()
+            plotly_fig = go.Figure()
 
-        # Add trace
-        plotly_trace = go.Scatter(x=timestamps, y=closing_prices, mode='lines+markers', name='Closing Price')
-        plotly_fig.add_trace(plotly_trace)
+            # Add trace
+            plotly_trace = go.Scatter(x=timestamps, y=closing_prices, mode='lines+markers', name='Closing Price')
+            plotly_fig.add_trace(plotly_trace)
 
-        # Layout adjustments
-        plotly_fig.update_layout(
-            title=f'{stock_symbol} Stock Price (5min intervals)',
-            xaxis_title='Timestamp',
-            yaxis_title='Closing Price'
-        )
+            # Layout adjustments
+            plotly_fig.update_layout(
+                title=f'{stock_symbol} Stock Price (5min intervals)',
+                xaxis_title='Timestamp',
+                yaxis_title='Closing Price'
+            )
 
-        # Create graphJSON
-        graphJSON = plotly_fig.to_json()
+            # Create graphJSON
+            graphJSON = plotly_fig.to_json()
 
-        return graphJSON
+            return graphJSON
+        except Exception as e:
+            return render_template('/login.html', error="sorry something went wrong, please try again later.")
